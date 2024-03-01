aecd8916a23be85896eaf5020d208b7ea9d1ebe0
3dehrk <laurinzoss@gmail.com>
2024-03-01 10:34:46 +0100

    💋
    💋
    
    💋

--- build.bat
+++ build.bat
@@ -2,10 +2,8 @@
 
 set SFML_INCLUDE_PATH=SFML-2.6.0\include
 set SFML_LIB_PATH=SFML-2.6.0\lib
-set SOURCE_FILE=src\main.cpp
-set OUTPUT_FILE=bin\app.exe
 
-g++ -I"%SFML_INCLUDE_PATH%" -L"%SFML_LIB_PATH%" -o "%OUTPUT_FILE%" %SOURCE_FILE% -lsfml-graphics -lsfml-system -lsfml-window
+g++ -I"%SFML_INCLUDE_PATH%" -L"%SFML_LIB_PATH%" -o bin\app.exe main.cpp -lsfml-graphics -lsfml-system -lsfml-window
 
 if errorlevel 1 (
     pause
